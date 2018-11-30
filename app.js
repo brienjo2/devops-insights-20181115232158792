0acb34d77a02e30b1d5e89d6aa004228dc092415
brienjo2 <42077581+brienjo2@users.noreply.github.com>
2018-11-30 13:05:56 -0600

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ server.listen(port, function () {
     console.log('Weather Report listening on ' + url);
 });
 
+console.log('hey there');
+
 app.get("/", function(req, res) {
     return res.render('main');
 });
